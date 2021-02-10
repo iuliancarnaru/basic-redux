f2dc182941943c6143a45a718a626eb4e483659e
iuliancarnaru <iulian.carnaru@gmail.com>
2021-02-10 15:26:37 +0000

    memoized calls to users

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import jsonplaceholder from '../api/jsonPlaceholder';
 
 // Async action creators
@@ -12,6 +13,19 @@ import jsonplaceholder from '../api/jsonPlaceholder';
 //   };
 // };
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  // userIds.forEach((id) => dispatch(fetchUser(id)));
+
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach((id) => dispatch(fetchUser(id)))
+    .value();
+};
+
 export const fetchPosts = () => async (dispatch) => {
   // returns an array of objects
   const response = await jsonplaceholder.get('/posts');
@@ -19,7 +33,19 @@ export const fetchPosts = () => async (dispatch) => {
 };
 
 export const fetchUser = (id) => async (dispatch) => {
-  // returns an object
   const response = await jsonplaceholder.get(`/users/${id}`);
   dispatch({ type: 'FETCH_USER', payload: response.data });
 };
+
+/*
+
+// the downside is that we can't refetch for the same user id
+export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+
+// lodash memoization
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const response = await jsonplaceholder.get(`/users/${id}`);
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+});
+
+*/
